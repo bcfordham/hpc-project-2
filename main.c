55db25ad49b4a77ac817c5f4663e73b9fac9304e
Blake Fordham <22708346@student.uwa.edu.au>
2023-08-30 13:52:22 +0800

    Add simulation steps for eating and swimming
    Added some functions to do the majority of the simulation work. I
    haven't tested it much, but it compiles and runs.

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-#define NUM_FISH 100		// TODO: find a better way of changing the number of fish, e.g. from the command line
+#define NUM_FISH 100
 #define SQUARE_SIZE 200
 #define INITIAL_WEIGHT 1
+#define NUM_STEPS 100
 
 struct fish {
 	double x;
 	double y;
 	double w;
+	double f;
 };
 
 // Initialises the fish position with random values and sets the weight to the intial weight
@@ -29,12 +32,70 @@ void print_fish(struct fish *fish) {
 	}
 }
 
+// Calculates the new weight for the fish
+void eat(struct fish *fish, double max, int step) {
+	if (!step) {
+		for (int i = 0; i < NUM_FISH; i++)
+			fish[i].w += (rand() / (double)RAND_MAX) * 0.01;	// TODO: potentially tweak this random value that stands in for the objective function stuff
+
+		return;
+	}
+
+	for (int i = 0; i < NUM_FISH; i++) {
+		if (fish[i].w < 2 * INITIAL_WEIGHT) {
+			fish[i].w += fish[i].f / max;
+
+			if(fish[i].w > 2 * INITIAL_WEIGHT)
+				fish[i].w = 2 * INITIAL_WEIGHT;
+		}
+	}
+}
+
+// Calculates the difference in the objective function from last step to this step and stores the result in the fish
+double obj_diff(struct fish fish, double prev_x, double prev_y) {
+	fish.f = fabs(sqrt(pow(fish.x, 2) + pow(fish.y, 2)) - sqrt(pow(prev_x, 2) + pow(prev_y, 2)));
+
+	return fish.f;
+}
+
+// Updates the locations of fish if they are below 2w, also determines the maximum change in f across fish for use in eat()
+double swim(struct fish *fish) {
+	double prev_x;
+	double prev_y;
+	double max = 0;
+	double new;
+
+	for (int i = 0; i < NUM_FISH; i++) {
+		if (fish[i].w < 2 * INITIAL_WEIGHT) {
+			prev_x = fish[i].x;
+			prev_y = fish[i].y;
+			fish[i].x += ((rand() / (double)RAND_MAX) - 0.5) * 0.2;
+			fish[i].y += ((rand() / (double)RAND_MAX) - 0.5) * 0.2;
+
+			if ((new = obj_diff(fish[i], prev_x, prev_y)) > max)
+				max = new;
+		}
+	}
+
+	return max;
+}
+
+void simulate(struct fish *fish) {
+	double max;
+
+	for (int i = 0; i < NUM_STEPS; i++) {
+		max = swim(fish);
+		eat(fish, max, i);
+	}
+}
+
 int main() {
 	struct fish *fish = malloc(NUM_FISH * sizeof(struct fish));
 
 	generate_fish(fish);
+	simulate(fish);
 	print_fish(fish);
-	
+
 	free(fish);
 
 	return 0;
