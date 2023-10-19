a841bd9bdff0d43113268f054acaf9b9eac3bee0
Blake Fordham <22708346@student.uwa.edu.au>
2023-10-19 11:32:59 +0800

    Remove unnecessary files

--- src/parallel_for_mpi.h
+++
@@ -1,4 +0,0 @@
-#include "fish.h"  // Ensure this includes the definition of the 'struct fish'
-void eat(struct fish *fish, double max, int step);
-double swim(struct fish *fish);
-double find_barycentre(struct fish *fish, double *local_numerator, double *local_denominator);
--- src/utilities_mpi.h
+++
@@ -1,4 +0,0 @@
-void generate_fish(struct fish *fish);
-void print_fish(struct fish fish, int num);
-void print_all_fish(struct fish *fish, double barycentre);
-double obj_diff(struct fish *fish, double prev_x, double prev_y);
