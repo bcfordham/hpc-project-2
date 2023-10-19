e21e9ada3a4239f8194e9c16a7e3dbc245ea5454
Blake Fordham <22708346@student.uwa.edu.au>
2023-10-19 11:39:30 +0800

    Fix point-to-point comms
    The for loop that looped over the MPI_Send function was hard coded at 4
    processes and we had been testing on Setonix with 2, that's why it was
    throwing errors. I just updated the loop to depend on NUM_PROCESSES.

--- src/main_2.c
+++ src/main_2.c
@@ -1,11 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <mpi.h>
-#include <unistd.h>
 #include "fish.h"
 #include "utilities.h"
-
-
 
 int main(int argc, char *argv[])
 {
@@ -20,6 +17,7 @@ int main(int argc, char *argv[])
 	double global_denominator;
 	double final_barycentre;
 	MPI_Status status;
+	
 	// MPI code initialisation
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -34,16 +32,15 @@ int main(int argc, char *argv[])
 	
     worker_fish = malloc(num_bytes);
 	if (rank == 0) {
-	for (int i = 1; i < 4; i++)
-		MPI_Send(&all_fish[(NUM_FISH / NUM_PROCESSES) * i], num_bytes, MPI_BYTE, i, i, MPI_COMM_WORLD);
+		for (int i = 1; i < NUM_PROCESSES; i++)
+			MPI_Send(&all_fish[(NUM_FISH / NUM_PROCESSES) * i], num_bytes, 
+				MPI_BYTE, i, i, MPI_COMM_WORLD);
 	}
-	if (rank != 0) {
+	if (rank != 0)
 		MPI_Recv(worker_fish, num_bytes, MPI_BYTE, 0, rank, MPI_COMM_WORLD, &status);
-	}
 
 	for (int i = 0; i < NUM_STEPS; i++) {
 		local_max = swim(worker_fish);
-		printf("%d is running\n", i);
 		MPI_Allreduce(
 			&local_max,
 			&global_max,
@@ -56,19 +53,15 @@ int main(int argc, char *argv[])
 		find_barycentre(worker_fish, &local_numerator, &local_denominator);
 		MPI_Reduce(&local_numerator, &global_numerator, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 		MPI_Reduce(&local_denominator, &global_denominator, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-		printf("barycentre is done for %d \n", i);
 		
 		if (rank == 0) 
 			final_barycentre = global_numerator / global_denominator;
-
-		printf("final barycentre is done: \n", final_barycentre);
 	}
 
 	MPI_Gather(worker_fish, num_bytes, MPI_BYTE, all_fish, num_bytes, MPI_BYTE, 0, MPI_COMM_WORLD);
 
-    if (rank == 0) {
+    if (rank == 0)
         free(all_fish);
-    }
 
     free(worker_fish);
 
