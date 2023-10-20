d85567c964b0510e8607119939b2729aaa991643
Blake Fordham <22708346@student.uwa.edu.au>
2023-10-20 15:26:47 +0800

    Add results from point_to_point experiment
    The naming convention is <number of nodes>N<number of threads>T.sh/out

---
+++ results/point_to_point/sh_files/run1N4T.sh
@@ -0,0 +1,14 @@
+#!/bin/sh 
+#SBATCH --account=courses0101
+#SBATCH --partition=debug
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:15:00
+#SBATCH --job-name=1N4T
+#SBATCH --output=1N4T.out
+#SBATCH --mem-per-cpu=32G
+#module load  openmpi/4.0.5
+
+export OMP_NUM_THREADS=4
+
+srun ./1N
---
+++ results/point_to_point/sh_files/run3N16T.sh
@@ -0,0 +1,14 @@
+#!/bin/sh 
+#SBATCH --account=courses0101
+#SBATCH --partition=debug
+#SBATCH --ntasks=3
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:15:00
+#SBATCH --job-name=3N16T
+#SBATCH --output=3N16T.out
+#SBATCH --mem-per-cpu=32G
+#module load  openmpi/4.0.5
+
+export OMP_NUM_THREADS=16
+
+srun ./3N
